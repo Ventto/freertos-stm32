98935cbc2a23a74d777a68f321a925c2a0dd12b1
Franklin "Snaipe" Mathieu <snaipe@diacritic.io>
2016-12-19 21:55:40 +0100

    usart: Moved message defs outside of implementation

--- src/usart.h
+++ src/usart.h
@@ -1,6 +1,21 @@
 #ifndef USART_H_
 #define USART_H_
 
+typedef enum E_USART_Msg_type {
+    USART_MSG_BUTTON_PUSHED,
+    USART_MSG_ADC_CONVERTION,
+    USART_MSG_CHAR_RECEVIED,
+    USART_MSG_I2C_VALUE,
+    USART_MSG_LAST
+} USART_Msg_type;
+
+typedef struct S_USART_Msg {
+    USART_Msg_type msg_type;
+    uint16_t adc;
+    uint16_t temp;
+    char usart;
+} USART_Msg;
+
 void initUSART(void);
 void usart_task_handleevents(void *pvParameters);
 
